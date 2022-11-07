43d74f41af0e377d65a6c72236925a751ae3dedd
Luke-Longo <lukelongo0421@gmail.com>
2022-11-07 12:20:26 -0500

    added shdwaccount and anchor walet

--- src/shdwDrive.ts
+++ src/shdwDrive.ts
@@ -1,37 +1,25 @@
 import { ShdwDrive } from "@shadow-drive/sdk";
-
 import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
-
+import anchor from "@project-serum/anchor";
 import * as dotenv from "dotenv";
-
-dotenv.config();
-
 import fs from "fs";
+dotenv.config();
 
 const loadKeypair = (filename: string): Keypair => {
 	const secret = JSON.parse(fs.readFileSync(filename).toString()) as number[];
 	const secretKey = Uint8Array.from(secret);
 	return Keypair.fromSecretKey(secretKey);
 };
-
-const connection = new Connection(clusterApiUrl("devnet"));
-const wallet = Keypair.generate();
-
-const drive = await new ShdwDrive(connection, wallet).init();
-
-const res = await drive.createStorageAccount("test", "444MB", "v2");
+// get the keypair and create anchor wallet
+const kp = loadKeypair("../keypair.json");
+const wallet = new anchor.Wallet(kp);
 
 const createAccount = async (): Promise<string> => {
 	const connection = new Connection(clusterApiUrl("devnet"));
-	const wallet = Keypair.generate();
 
 	const drive = await new ShdwDrive(connection, wallet).init();
-
 	const resp = await drive.createStorageAccount("test", "444MB", "v2");
-
 	return resp.shdw_bucket;
 };
-
-const kp = loadKeypair("../keypair.json");
 
 const bucket = createAccount();
