271182e174463dec460954bac42753e2ff4e062b
Luke-Longo <lukelongo0421@gmail.com>
2022-11-07 12:31:34 -0500

    setup properly

--- src/shdwDrive.ts
+++ src/shdwDrive.ts
@@ -1,6 +1,6 @@
 import { ShdwDrive } from "@shadow-drive/sdk";
 import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
-import anchor from "@project-serum/anchor";
+import * as anchor from "@project-serum/anchor";
 import * as dotenv from "dotenv";
 import fs from "fs";
 dotenv.config();
@@ -11,15 +11,18 @@ const loadKeypair = (filename: string): Keypair => {
 	return Keypair.fromSecretKey(secretKey);
 };
 // get the keypair and create anchor wallet
-const kp = loadKeypair("../keypair.json");
+const kp = loadKeypair("./keypair.json");
 const wallet = new anchor.Wallet(kp);
 
 const createAccount = async (): Promise<string> => {
-	const connection = new Connection(clusterApiUrl("devnet"));
+	const connection = new Connection(clusterApiUrl("mainnet-beta"));
 
 	const drive = await new ShdwDrive(connection, wallet).init();
 	const resp = await drive.createStorageAccount("test", "444MB", "v2");
+	console.log(resp.transaction_signature);
 	return resp.shdw_bucket;
 };
 
-const bucket = createAccount();
+const bucket = createAccount().then((bucket) => {
+	console.log(bucket);
+});
