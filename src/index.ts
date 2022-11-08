f413d17939967b5422a1c3cc450a6251d044b788
Luke-Longo <lukelongo0421@gmail.com>
2022-11-08 11:48:30 -0500

    added guards

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import {
 	keypairIdentity,
 	bundlrStorage,
 	toBigNumber,
+	CreateCandyMachineInput,
+	sol,
+	toDateTime,
 } from "@metaplex-foundation/js";
-import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
+import { Connection, clusterApiUrl, Keypair, PublicKey } from "@solana/web3.js";
 import fs from "fs";
 
 const main = async () => {
@@ -15,63 +18,58 @@ const main = async () => {
 	};
 
 	// get the keypair and create anchor wallet
-	const kp = loadKeypair("./keypair.json");
+	const collectionAuthority = loadKeypair("./keypair.json");
 
 	const connection = new Connection(clusterApiUrl("devnet"));
-	const collectionAuthority = kp;
 
 	const metaplex = Metaplex.make(connection)
-		.use(keypairIdentity(kp))
+		.use(keypairIdentity(collectionAuthority))
 		.use(bundlrStorage());
 
-	const candyMachineSettings = {
+	const { nft: collectionNft } = await metaplex.nfts().create({
+		name: "Elegant Eyes",
+		uri: "https://shdw-drive.genesysgo.net/7ne9NYWDM62CjM6Y6Z9VrFDBktvHjeb24rWKw8epZMMZ/0.json",
+		sellerFeeBasisPoints: 0,
+		isCollection: true,
+		updateAuthority: collectionAuthority,
+	});
+
+	const candyMachineSettings: CreateCandyMachineInput = {
 		itemsAvailable: toBigNumber(10),
 		sellerFeeBasisPoints: 100, // 3.33%
-		// collection: {
-		// 	address: collectionNft.address,
-		// 	updateAuthority: collectionAuthority,
-		// },
-		itemSettings: {
-			type: "hidden",
-			name: "Dingus",
-			symbol: "DING",
-			uri: "https://example.com",
-			hash: "Qm...",
-			image:
-				"https://shdw-drive.genesysgo.net/7ne9NYWDM62CjM6Y6Z9VrFDBktvHjeb24rWKw8epZMMZ/0.png",
-			creators: [
-				{
-					address: collectionAuthority.publicKey.toBase58(),
-					share: 100,
-				},
-			],
-			properties: {
-				files: [
-					{
-						uri: "https://shdw-drive.genesysgo.net/7ne9NYWDM62CjM6Y6Z9VrFDBktvHjeb24rWKw8epZMMZ/0.png",
-						type: "image/png",
-					},
-				],
+		collection: {
+			address: collectionNft.address,
+			updateAuthority: collectionAuthority,
+		},
+		symbol: "ELEY",
+		creators: [
+			{
+				address: collectionAuthority.publicKey,
+				share: 100,
 			},
+		],
+		isMutable: true,
+		itemSettings: {
+			type: "configLines",
+			prefixName: "Elegant Eye #$ID+1$",
+			nameLength: 0,
+			prefixUri:
+				"https://shdw-drive.genesysgo.net/7ne9NYWDM62CjM6Y6Z9VrFDBktvHjeb24rWKw8epZMMZ/#$ID+1$.json",
+			uriLength: 0,
+			isSequential: false,
+		},
+		guards: {
+			solPayment: { amount: sol(0.1), destination: collectionAuthority.publicKey },
+			startDate: { date: toDateTime("2022-11-06T13:37:00Z") },
+			// all other guards are disabled
 		},
 	};
 
-	// const { candyMachine } = await metaplex
-	// 	.candyMachines()
-	// 	.create(candyMachineSettings);
+	const { candyMachine } = await metaplex
+		.candyMachines()
+		.create(candyMachineSettings);
 
-	const collectionNft = "9fhUjHJctHx9jfmoPVPxKWNLub95ePaVeupBTt3mNHHu";
+	console.log("Candy machine address:", candyMachine.address.toBase58());
 };
 
 main();
-
-// const { nft: collectionNft } = await metaplex.nfts().create({
-// 	sellerFeeBasisPoints: 0,
-// 	name: "Dingus",
-// 	symbol: "DING",
-// 	uri: "https://shdw-drive.genesysgo.net/7ne9NYWDM62CjM6Y6Z9VrFDBktvHjeb24rWKw8epZMMZ/0.json",
-// 	isCollection: true,
-// 	updateAuthority: collectionAuthority,
-// });
-
-// console.log("collectionNft", collectionNft.address.toBase58());
--- src/shdwDrive.ts
+++ src/shdwDrive.ts
@@ -4,7 +4,6 @@ import * as anchor from "@project-serum/anchor";
 import * as dotenv from "dotenv";
 import fs from "fs";
 dotenv.config();
-
 const loadKeypair = (filename: string): Keypair => {
 	const secret = JSON.parse(fs.readFileSync(filename).toString()) as number[];
 	const secretKey = Uint8Array.from(secret);
@@ -14,29 +13,40 @@ const loadKeypair = (filename: string): Keypair => {
 const kp = loadKeypair("./keypair.json");
 const wallet = new anchor.Wallet(kp);
 const connection = new Connection(clusterApiUrl("mainnet-beta"), "max");
-
-const createAccount = async (
+export const createAccount = async (
 	name: string,
 	storage: string,
 	version: "v1" | "v2"
 ): Promise<string> => {
+	const loadKeypair = (filename: string): Keypair => {
+		const secret = JSON.parse(fs.readFileSync(filename).toString()) as number[];
+		const secretKey = Uint8Array.from(secret);
+		return Keypair.fromSecretKey(secretKey);
+	};
+	// get the keypair and create anchor wallet
+	const kp = loadKeypair("./keypair.json");
+	const wallet = new anchor.Wallet(kp);
+	const connection = new Connection(clusterApiUrl("mainnet-beta"), "max");
+
 	const drive = await new ShdwDrive(connection, wallet).init();
-	const resp = await drive.createStorageAccount("test", "333MB", "v2");
+	const resp = await drive.createStorageAccount(name, storage, version);
 	console.log("Storage account tx: ", resp.transaction_signature);
 	console.log("bucket: ", resp.shdw_bucket);
 	return resp.shdw_bucket;
 };
 
 const bucket = "7ne9NYWDM62CjM6Y6Z9VrFDBktvHjeb24rWKw8epZMMZ";
-const uploadCollectionImage = async () => {
+export const uploadCollectionSingle = async (
+	name: string,
+	type: "json" | "png"
+): Promise<string> => {
 	const drive = await new ShdwDrive(connection, wallet).init();
-	const fileBuff = fs.readFileSync("./assets/0.json");
+	const fileBuff = fs.readFileSync(`./assets/${name}.${type}`);
 	const fileToUpload: ShadowFile = {
-		name: "0.json",
+		name,
 		file: fileBuff,
 	};
 	const res = await drive.uploadFile(new PublicKey(bucket), fileToUpload, "v2");
 	console.log("upload tx: ", res.finalized_locations[0]);
+	return res.finalized_locations[0];
 };
-
-uploadCollectionImage();
